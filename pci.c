fed1c29938b720880db8834e1381af4bd8f20f66
Sajini R <quic_sajinir@quicinc.com>
2021-11-12 17:04:12 +0530

    cnss2: Fix vap up issue due to PCI region request failure in SSR flow
    In SSR recovery, after 200+ iterations, it is seen that vap up fails due
    to pci request region failure. Generally, ramdump collection fails when
    ramdump device creation fails if device/resource is busy. If ramdump
    collection fails, then the device shutdown process that was stalled for
    ramdump collection would not go through when resumed. This is because,
    "dump_data_valid" will still be set to true as it will be cleared to false
    only upon successful ramdump collection. If shutdown doesn't happen, then
    the subsequent power up that happens as part of SSR recovery flow would
    result in pci region request failure due to device/resource being busy.
    This change makes sure that dump_data_valid is cleared regardless of the
    status of ramdump collection to ensure graceful exit in case ramdump
    collection fails.
    
    Change-Id: I53a699c74fc045ad2eb105648d127c7d686ad1c6
    Signed-off-by: Sajini R <quic_sajinir@quicinc.com>

--- pci.c
+++ pci.c
@@ -1818,15 +1818,14 @@ static int cnss_qcn9000_ramdump(struct cnss_pci_data *pci_priv)
 
 #ifdef CONFIG_CNSS2_KERNEL_IPQ
 	ret = create_ramdump_device_file(info_v2->ramdump_dev);
-	if (ret) {
-		kfree(meta_info);
-		kfree(ramdump_segs);
-		return ret;
-	}
 #endif
+	if (ret)
+		goto clear_dump_info;
 
 	ret = do_elf_ramdump(info_v2->ramdump_dev, ramdump_segs,
 			     dump_data->nentries + CNSS_NUM_META_INFO_SEGMENTS);
+
+clear_dump_info:
 	kfree(meta_info);
 	kfree(ramdump_segs);
 
