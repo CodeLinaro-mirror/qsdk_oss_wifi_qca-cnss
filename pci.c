6563a515cae7287aba7b8c9bc2567037bfa0813f
Vignesh Viswanathan <quic_viswanat@quicinc.com>
2022-01-21 21:16:54 +0530

    cnss2: Take PM Vote so that QCN9000 never goes to power collapse
    To avoid QCN9000 and the NTN3 switch from going to low power mode, take
    PM vote as soon as QCN9000 is enumerated.
    
    Change-Id: I701c281b51c96e47f2214d7633987b982890ce8f
    Signed-off-by: Vignesh Viswanathan <quic_viswanat@quicinc.com>
    Signed-off-by: Hariharan Ramanathan <quic_hramanat@quicinc.com>

--- pci.c
+++ pci.c
@@ -5933,6 +5933,12 @@ int cnss_pci_probe_basic(struct pci_dev *pci_dev,
 	}
 #endif
 
+#ifdef CONFIG_CNSS2_KERNEL_MSM
+	cnss_pr_info("Taking PM vote for %s", plat_priv->device_name);
+	device_set_wakeup_enable(&pci_dev->dev, true);
+	pm_stay_awake(&pci_dev->dev);
+#endif
+
 	cnss_pr_info("PCI device %pK probed successfully\n",
 		     plat_priv->pci_dev);
 
