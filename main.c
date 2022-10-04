099d240dd6692ffcbb292a31d3f9198f536c5b2e
Madhavan Ganesan <quic_madhgane@quicinc.com>
2022-10-04 08:07:43 +0530

    qca-cnss: Change probe type to Sync when registering platform driver
    For SDX 2xPine, the compatible string is matching for both the Pines and
    causing the probe order changes after a few reboot iterations.
    This is because we are registering the platform driver as an Asynchronous
    type and the VAPs are not coming up due to a mismatch between probe order
    change and the wireless configuration.
    
    Change probe type to Synchronous type when registering the platform driver
    to avoid probe order change
    
    Signed-off-by: Madhavan Ganesan <quic_madhgane@quicinc.com>
    Change-Id: Idecd8b96a79cd09434697a9244a56baf48592a1d

--- main.c
+++ main.c
@@ -4042,9 +4042,7 @@ static struct platform_driver cnss_platform_driver = {
 	.driver = {
 		.name = "cnss2",
 		.of_match_table = cnss_of_match_table,
-#ifdef CONFIG_CNSS_ASYNC
-		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
-#endif
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
