637f030f4f59ad7467beb403c978cac069154887
Sajini R <quic_sajinir@quicinc.com>
2022-12-06 12:19:55 +0530

    cnss2: Honor QDSS trace save indication for IPQ5332
    Changes to honor QDSS trace save indication for IPQ5332.
    Going forward, all 11be family chipsets, whether they're integrated
    or PCI radios, would have QDSS flow similar to that of 11ax family
    PCI devices. That is, QDSS would be started soon after mode message
    is sent to FW and upon QDSS stop, trace file is required to be saved
    in /data/vendor/wifi.
    
    Change-Id: I8582b10b5421ee55abe214d199040f14f7de3cce
    Signed-off-by: Sajini R <quic_sajinir@quicinc.com>

--- bus.c
+++ bus.c
@@ -249,7 +249,8 @@ int cnss_bus_alloc_qdss_mem(struct cnss_plat_data *plat_priv)
 			plat_priv->qdss_mem[i].size = resource_size(&q6_etr);
 			plat_priv->qdss_mem[i].type = QMI_WLFW_MEM_QDSS_V01;
 
-			if (plat_priv->device_id == QCN6122_DEVICE_ID) {
+			if (plat_priv->device_id == QCN6122_DEVICE_ID ||
+			    plat_priv->device_id == QCA5332_DEVICE_ID) {
 				plat_priv->qdss_mem[i].va =
 					ioremap(plat_priv->qdss_mem[i].pa,
 						plat_priv->qdss_mem[i].size);
