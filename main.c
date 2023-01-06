4adef186db31cf05264d69fef4776f1b1b993b8d
Madhavan Ganesan <quic_madhgane@quicinc.com>
2023-01-06 13:06:41 +0530

    qca-cnss: Change the hw link IDs to sequential order
    Change the hw link IDs to sequential order 0,1,2 instead of 0,2,4
    
    Change-Id: I84add78ad04ee7f7c77a3ef0f4bed9208b844f67
    Signed-off-by: Madhavan Ganesan <quic_madhgane@quicinc.com>

--- main.c
+++ main.c
@@ -1678,15 +1678,17 @@ void cnss_set_default_mlo_config(void)
 					ch_info->hw_link_ids[k] = link_id + k;
 					ch_info->valid_link_ids[k] = 1;
 				}
+				grp_link_id[group_id] = grp_link_id[group_id] +
+							CNSS_MAX_LINKS_PER_CHIP;
 			} else {
 				ch_info->num_local_links = 1;
 				ch_info->hw_link_ids[0] = link_id;
 				ch_info->valid_link_ids[0] = 1;
 				ch_info->valid_link_ids[1] = 0;
+				grp_link_id[group_id] = grp_link_id[group_id] +
+									1;
 			}
 
-			grp_link_id[group_id] = grp_link_id[group_id] +
-							CNSS_MAX_LINKS_PER_CHIP;
 			grp_chip_id[group_id] = grp_chip_id[group_id] + 1;
 		}
 
