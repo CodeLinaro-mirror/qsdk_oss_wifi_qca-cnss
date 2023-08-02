9449cabde6bf264110fab95309e6c1c50ed2df8b
Madhavan Ganesan <quic_madhgane@quicinc.com>
2023-08-02 18:02:07 +0530

    qca-cnss: Use PCI domain ID instead of qrtr_node_id
    qrtr_node_id is used to map the wifi node and PCI devices.
    
    From 6.x kernel remove the qrtr_node_id and use the PCI domain ID
    plus corresponding base to get the original qrtr node ID for mapping.
    
    Change-Id: I901df9cd6de569a1629ef765eb4e53e42204dfeb
    Signed-off-by: Madhavan Ganesan <quic_madhgane@quicinc.com>

--- main.c
+++ main.c
@@ -2909,6 +2909,33 @@ static int cnss_rproc_start(struct cnss_plat_data *plat_priv)
 	}
 	return 0;
 }
+
+static int cnss_get_node_id(struct platform_device *plat_dev,
+			    unsigned long device_id, u32 *node_id)
+{
+	struct cnss_plat_data *plat_priv = NULL;
+
+	if (of_property_read_u32(plat_dev->dev.of_node,
+				 "node_id", node_id)) {
+		cnss_pr_err("Error: No node_id in device_tree\n");
+		CNSS_ASSERT(0);
+		return -ENODEV;
+	}
+
+	switch (device_id) {
+	case QCN9000_DEVICE_ID:
+		*node_id = *node_id + (QCN9000_0 - 1);
+		break;
+	case QCN9224_DEVICE_ID:
+		*node_id = *node_id + (QCN9224_0 - 1);
+		break;
+	default:
+		cnss_pr_dbg("Invalid device id 0x%lx", device_id);
+		break;
+	}
+
+	return 0;
+}
 #else
 int cnss_stop_rproc(struct cnss_plat_data *plat_priv, struct rproc *rproc)
 {
@@ -3135,6 +3162,22 @@ static int cnss_qcn9000_notifier_nb(struct notifier_block *nb,
 
 	return NOTIFY_OK;
 }
+
+static int cnss_get_node_id(struct platform_device *plat_dev,
+			    unsigned long device_id, u32 *node_id)
+{
+	struct cnss_plat_data *plat_priv = NULL;
+
+	if (of_property_read_u32(plat_dev->dev.of_node,
+				 "qrtr_node_id", node_id)) {
+		cnss_pr_err("Error: No qrtr_node_id in device_tree\n");
+		CNSS_ASSERT(0);
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
 #endif
 
 void  *__cnss_subsystem_get(struct cnss_plat_data *plat_priv)
@@ -6876,15 +6919,12 @@ static int cnss_probe(struct platform_device *plat_dev)
 		goto out;
 	}
 
-	if ((device_id->driver_data == QCN9000_DEVICE_ID ||
-	     device_id->driver_data == QCN9224_DEVICE_ID) &&
-	    (of_property_read_u32(plat_dev->dev.of_node,
-				  "qrtr_node_id", &node_id))) {
-		pr_err("Error: No qrtr_node_id in device_tree\n");
-		CNSS_ASSERT(0);
-		ret = -ENODEV;
-		goto out;
-	}
+	if (device_id->driver_data == QCN9000_DEVICE_ID ||
+	     device_id->driver_data == QCN9224_DEVICE_ID)
+		if (cnss_get_node_id(plat_dev, device_id->driver_data,
+				     &node_id))
+			goto out;
+
 #ifdef CONFIG_QCOM_SOCINFO
 #ifdef CONFIG_CNSS2_KERNEL_IPQ
 	/* Check for QCA9574 here and skip probe accordingly */
