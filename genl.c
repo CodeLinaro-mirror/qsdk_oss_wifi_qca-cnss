61e9db9c84ae7c135f6b190b7e2774648623be44
Miaoqing Pan <quic_miaoqing@quicinc.com>
2023-09-04 16:11:43 +0800

    qca-cnss: update generic netlink name
    Update generic netlink name to avoid conflict with cnss2.
    
    Change-Id: If24fdd0cc38c91014102f75dfcc389481f0ad108
    Signed-off-by: Miaoqing Pan <quic_miaoqing@quicinc.com>

--- genl.c
+++ genl.c
@@ -19,8 +19,8 @@
 #include "main.h"
 #include "debug.h"
 
-#define CNSS_GENL_FAMILY_NAME "cnss-genl"
-#define CNSS_GENL_MCAST_GROUP_NAME "cnss-genl-grp"
+#define CNSS_GENL_FAMILY_NAME "qca-cnss-genl"
+#define CNSS_GENL_MCAST_GROUP_NAME "qca-cnss-genl-g"
 #define CNSS_GENL_VERSION 1
 #define CNSS_GENL_DATA_LEN_MAX 4000
 
