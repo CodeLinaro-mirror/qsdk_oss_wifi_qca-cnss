d39e3a3be9208c12330a0bf59f96c5104e12def6
Raghavendran Madhavan <quic_raghmadh@quicinc.com>
2023-10-06 10:46:21 +0530

    qca-cnss: Remove q6 timestamp print
    Removing the q6 timestamp print to avoid flooding
    of error prints in standby mode.
    
    Change-Id: I7a3881e1199079bc0cc1dbc0d52d8a01d90f4d85
    Signed-off-by: Raghavendran Madhavan <quic_raghmadh@quicinc.com>

--- pci.c
+++ pci.c
@@ -1769,11 +1769,8 @@ u64 cnss_get_q6_time(struct device *dev)
 		return 0;
 	}
 
-	if (!test_bit(CNSS_FW_READY, &plat_priv->driver_state)) {
-		cnss_pr_err("Invalid state to get the Q6 timestamp: 0x%lx\n",
-			    plat_priv->driver_state);
+	if (!test_bit(CNSS_FW_READY, &plat_priv->driver_state))
 		return 0;
-	}
 
 	switch (plat_priv->bus_type) {
 	case CNSS_BUS_PCI:
