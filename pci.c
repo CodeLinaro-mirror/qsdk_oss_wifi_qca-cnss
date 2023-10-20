199159801b6839219d9a71853657cff9dbe7054c
Ashishkumar Vijayakumar <quic_ashivija@quicinc.com>
2023-10-20 17:55:16 +0530

    qca-cnss: Use PCI domain ID instead of qrtr_node_id
    qrtr_node_id is used to map the wifi node and PCI devices.
    
    For 5.15 kernel remove the qrtr_node_id and use the PCI domain ID
    plus corresponding base to get the original qrtr node ID for mapping.
    
    Change-Id: I901df9cd6de569a1629ef765eb4e53e42204dfeb
    Signed-off-by: Ashishkumar Vijayakumar <quic_ashivija@quicinc.com>

--- pci.c
+++ pci.c
@@ -2143,11 +2143,48 @@ static int cnss_qca6174_ramdump(struct cnss_pci_data *pci_priv)
 	ret = do_ramdump(ramdump_info->ramdump_dev, &segment, 1);
 	return ret;
 }
+
+static int cnss_get_qrtr_instance_id(struct pci_dev *pci_dev, u32 *node_id)
+{
+	int ret;
+
+	ret = of_property_read_u32(pci_dev->dev.of_node,
+				   "qrtr_node_id", node_id);
+	if (ret) {
+		pr_err("Failed to get Instance ID %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
 #else
 static int cnss_qca6174_ramdump(struct cnss_pci_data *pci_priv)
 {
 	return 0;
 }
+
+static int cnss_get_qrtr_instance_id(struct pci_dev *pci_dev, u32 *node_id)
+{
+	struct cnss_plat_data *plat_priv = NULL;
+	*node_id = (pci_domain_nr(pci_dev->bus) & 0xF);
+
+	switch (pci_dev->device) {
+	case QCN9000_DEVICE_ID:
+		*node_id = *node_id + QCN9000_0;
+		break;
+	case QCN9224_DEVICE_ID:
+		*node_id = *node_id + QCN9224_0;
+		break;
+	default:
+		cnss_pr_dbg("Invalid device id 0x%lx",
+			     (unsigned long)pci_dev->device);
+		break;
+	}
+
+	return 0;
+}
+
 #endif
 
 static int cnss_qcn9000_powerup(struct cnss_pci_data *pci_priv)
@@ -7396,12 +7433,8 @@ int cnss_pci_probe_basic(struct pci_dev *pci_dev,
 		/* Temporarily look for qrtr_node_id as fallback for QCN9224
 		 * till we have a better way to link pci_dev to plat_priv
 		 */
-		ret = of_property_read_u32(pci_dev->dev.of_node,
-					   "qrtr_node_id", &qrtr_instance);
-		if (ret) {
-			pr_err("Failed to get Instance ID %d\n", ret);
+		if (cnss_get_qrtr_instance_id(pci_dev, &qrtr_instance))
 			return ret;
-		}
 	}
 
 	plat_priv = cnss_get_plat_priv_by_qrtr_node_id(qrtr_instance);
@@ -7442,7 +7475,7 @@ int cnss_pci_probe_basic(struct pci_dev *pci_dev,
 void cnss_pci_remove_basic(struct pci_dev *pci_dev)
 {
 	struct cnss_plat_data *plat_priv = NULL;
-	u32 qrtr_instance;
+	u32 qrtr_instance = 0;
 	int ret;
 
 	ret = of_property_read_u32(pci_dev->dev.of_node,
@@ -7451,12 +7484,8 @@ void cnss_pci_remove_basic(struct pci_dev *pci_dev)
 		/* Temporarily look for qrtr_node_id as fallback for QCN9224
 		 * till we have a better way to link pci_dev to plat_priv
 		 */
-		ret = of_property_read_u32(pci_dev->dev.of_node,
-					   "qrtr_node_id", &qrtr_instance);
-		if (ret) {
-			pr_err("Failed to get Instance ID %d\n", ret);
+		if (cnss_get_qrtr_instance_id(pci_dev, &qrtr_instance))
 			return;
-		}
 	}
 
 	plat_priv = cnss_get_plat_priv_by_qrtr_node_id(qrtr_instance);
