b3eae68361e019bfc35d0444fe9dd31d08ad85f4
Kannan Saravanan <quic_kannsara@quicinc.com>
2023-11-09 20:43:39 +0530

    qca-cnss: Update get PCI slot id for QCN6432
    Adding changes to get PCI slot id for QCN6432 from DTS
    boot-args node.
    
    Change-Id: I382bbdbd9a2de5a3731bb9e21068348e5b3238b7
    Signed-off-by: Raghavendran Madhavan <quic_raghmadh@quicinc.com>

--- pci/pci.c
+++ pci/pci.c
@@ -4089,6 +4089,7 @@ int cnss_get_pci_slot(struct device *dev)
 {
 	struct cnss_plat_data *plat_priv =
 		cnss_bus_dev_to_plat_priv(dev);
+	uint32_t pci_slot_id = 0;
 
 	if (!plat_priv)
 		return -ENODEV;
@@ -4100,8 +4101,11 @@ int cnss_get_pci_slot(struct device *dev)
 		return plat_priv->qrtr_node_id - QCN9224_0;
 	case QCN6122_DEVICE_ID:
 	case QCN9160_DEVICE_ID:
-	case QCN6432_DEVICE_ID:
 		return plat_priv->userpd_id - USERPD_0;
+	case QCN6432_DEVICE_ID:
+		of_property_read_u32(dev->of_node, "qcom,pci_slot_id",
+					&pci_slot_id);
+		return pci_slot_id;
 default:
 		cnss_pr_info("PCI slot is 0 for target 0x%lx",
 			     plat_priv->device_id);
