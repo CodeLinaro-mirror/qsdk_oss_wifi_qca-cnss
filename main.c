82e3505472b6e8c399e682a51dcd98e413adb1b3
Ashishkumar Vijayakumar <quic_ashivija@quicinc.com>
2023-11-23 12:08:24 +0530

    qca-cnss: Add modparam support to update mlo_max_peer and fw_mem_mode.
    Add modparam support to update mlo_max_peer and fw_mem_mode.
    
    Change-Id: I673f80028738798d9473575d5b13d7cf2991be0d
    Signed-off-by: Ashishkumar Vijayakumar <quic_ashivija@quicinc.com>

--- main.c
+++ main.c
@@ -170,6 +170,10 @@ static int enable_intx_bmap;
 module_param(enable_intx_bmap, int, 0644);
 MODULE_PARM_DESC(enable_intx_bmap, "enable_intx_bmap");
 
+static unsigned int mlo_max_peer;
+module_param(mlo_max_peer, uint, 0600);
+MODULE_PARM_DESC(mlo_max_peer, "MLO max peer");
+
 #define FW_READY_DELAY	100  /* in msecs */
 
 /* In platforms with low power CPU like IPQ5018 or SDX65, if CPU load
@@ -1829,7 +1833,11 @@ void cnss_set_default_mlo_config(void)
 			return;
 		}
 		mlo_group_info[group_id].group_id = group_id;
-		mlo_group_info[group_id].max_num_peers = 256;
+		if (mlo_max_peer == 0)
+			mlo_group_info[group_id].max_num_peers = 256;
+		else
+			mlo_group_info[group_id].max_num_peers = mlo_max_peer;
+
 		if (mlo_chip_bitmask & (1 << i)) {
 			/*Temporarily Hard coding group id as 0 */
 			num_chip = grp_chip_id[group_id];
