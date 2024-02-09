7bb44285797705b8d3f9854bef344f08ca1b7451
Madhavan Ganesan <quic_madhgane@quicinc.com>
2024-02-09 17:41:47 +0530

    Revert "qca-cnss: add the RDDM check in down path for getting valid dump"
    This reverts commit b6e7667b7ecbc3b27101528ffbb19217ce579f7e.

--- pci/pci.c
+++ pci/pci.c
@@ -1735,32 +1735,24 @@ out:
 	return ret;
 }
 
-static int cnss_mhi_soc_reset(struct pci_dev *pci_dev)
+static void cnss_mhi_soc_reset(struct pci_dev *pci_dev)
 {
 	struct cnss_pci_data *pci_priv = cnss_get_pci_priv(pci_dev);
 	struct cnss_plat_data *plat_priv =
 		cnss_bus_dev_to_plat_priv(&pci_dev->dev);
-	int ret = 0;
 
 	if (test_bit(CNSS_MHI_RDDM, &pci_priv->mhi_state)) {
-		cnss_pr_err("MHI SOC_RESET is not required as MHI is already in RDDM state\n");
+		cnss_pr_info("MHI SOC_RESET is not required as MHI is already in RDDM state\n");
 		clear_bit(CNSS_MHI_RDDM, &pci_priv->mhi_state);
-		return -EBUSY;
+		return;
 	}
 
-	ret = cnss_pci_set_mhi_state(pci_priv, CNSS_MHI_SOC_RESET);
-	if (ret) {
-		cnss_pr_err("MHI SOC_RESET failed ret, %d\n", ret);
-		return ret;
-	}
+	cnss_pci_set_mhi_state(pci_priv, CNSS_MHI_SOC_RESET);
 	if (!wait_for_completion_timeout(&plat_priv->soc_reset_request_complete,
 				msecs_to_jiffies(MHI_SOC_RESET_DELAY))) {
 		cnss_pr_err("%s: Failed to switch RDDM state\n", __func__);
 		reinit_completion(&plat_priv->soc_reset_request_complete);
-		return -EBUSY;
 	}
-
-	return ret;
 }
 
 static int cnss_qcn9000_shutdown(struct cnss_pci_data *pci_priv)
@@ -1783,9 +1775,7 @@ static int cnss_qcn9000_shutdown(struct cnss_pci_data *pci_priv)
 		return ret;
 	}
 
-	ret = cnss_mhi_soc_reset(plat_priv->pci_dev);
-	if (ret)
-		return ret;
+	cnss_mhi_soc_reset(plat_priv->pci_dev);
 
 	cnss_pr_info("Shutting down %s\n", plat_priv->device_name);
 	cnss_pci_pm_runtime_resume(pci_priv);
@@ -4855,7 +4845,7 @@ static void cnss_mhi_notify_status(struct mhi_controller *mhi_ctrl,
 	if (reason == MHI_CB_EE_RDDM) {
 		/* In-case of RDDM switched by MHI SoC Reset */
 		if (test_bit(CNSS_MHI_SOC_RESET, &pci_priv->mhi_state)) {
-			cnss_pr_info("Switched from MHI SOC_RESET to RDDM state\n");
+			cnss_pr_dbg("Switched from MHI SOC_RESET to RDDM state\n");
 			complete(&plat_priv->soc_reset_request_complete);
 			clear_bit(CNSS_MHI_SOC_RESET, &pci_priv->mhi_state);
 			return;
